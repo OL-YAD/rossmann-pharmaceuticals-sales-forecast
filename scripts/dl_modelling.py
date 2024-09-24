fba2a77c54e2b4a6ac8ad95f6063ad6a33ad2c86
OL-YAD <olyadtemesgen@gmail.com>
2024-09-24 19:18:24 +0300

    logger added for lstm model

--- scripts/dl_modelling.py
+++ scripts/dl_modelling.py
@@ -8,7 +8,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout, Input
 from tensorflow.keras.optimizers import Adam
 
-
+from scripts.logger import setup_logger
+logger = setup_logger('dl_logger', '../logs/lstm.log')
 def check_stationarity(timeseries):
     """Check whether your time Series Data is Stationary."""
     result = adfuller(timeseries, autolag='AIC')
@@ -31,6 +32,9 @@ def create_supervised_data(data, n_step=1):
     for i in range(len(data) - n_step - 1):
         X.append(data[i:(i + n_step), 0])
         y.append(data[i + n_step, 0])
+
+        logger.info(f'Supervised data created with n_step = {n_step}')
+        logger.info(f'X shape: {np.array(X).shape}, y shape: {np.array(y).shape}')
     return np.array(X), np.array(y)
 
 
@@ -46,9 +50,13 @@ def build_lstm_model(n_step):
     model.add(Dense(1))
     
     model.compile(optimizer='adam', loss='mean_squared_error')
+
+    logger.info('LSTM model built with {} steps'.format(n_step))
     return model
 
 def train_lstm_model(model, X, y, epochs=50, batch_size=32, validation_split=0.2):
     # Fit the model and store the history
+    logger.info(f'Training LSTM model with epochs={epochs}, batch_size={batch_size}, validation_split={validation_split}')
     history = model.fit(X, y, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
+    logger.info('Model training complete')
     return history
